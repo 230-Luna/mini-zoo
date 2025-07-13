d2e04be5d4aefa03a5672f53b1b7c9b83208052f
230-Luna <lsy77877@gmail.com>
2025-07-13 15:15:39 +0900

    feat: colors setting

---
+++ src/utils/colors.ts
@@ -0,0 +1,45 @@
+export const colors = {
+  peach50: "#FFF2EB", // background
+  peach100: "#FAE3D5",
+  peach200: "#F5D6C6", // sub-background(card, section..)
+  peach300: "#EDC2AC",
+  peach400: "#E5AD93",
+  peach500: "#DB997C",
+  peach600: "#C97C60",
+  peach700: "#B0604A",
+  peach800: "#94493A",
+  peach900: "#79362D",
+
+  orange50: "#FFF3E5",
+  orange100: "#FFE0BA",
+  orange200: "#FFC58F",
+  orange300: "#FFA566",
+  orange400: "#FF893F",
+  orange500: "#FF7A1A", // emphasize-text(alert, warning)
+  orange600: "#DB5D12",
+  orange700: "#B7440D",
+  orange800: "#933208",
+  orange900: "#7A2705",
+
+  brown50: "#F3ECEB",
+  brown100: "#E2D4D3",
+  brown200: "#CFBAB7",
+  brown300: "#B89D98",
+  brown400: "#A07F7A",
+  brown500: "#88645F",
+  brown600: "#6F514C",
+  brown700: "#604743",
+  brown800: "#4F3B38",
+  brown900: "#574142", // button-background, text
+
+  white50: "#FFFFFF", // button-text
+  white100: "#FDFDFD",
+  white200: "#FAFAFA",
+  white300: "#F7F7F7",
+  white400: "#F3F3F3",
+  white500: "#EFEFEF",
+  white600: "#EAEAEA",
+  white700: "#E4E4E4",
+  white800: "#DEDEDE",
+  white900: "#D8D8D8",
+} as const;
