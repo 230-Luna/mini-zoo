ca1ea91ec14e3b786825cfd9d552de37b429d7c9
230-Luna <lsy77877@gmail.com>
2025-07-16 22:47:41 +0900

    fixed: Flex props 수정

--- src/components/Flex.tsx
+++ src/components/Flex.tsx
@@ -11,26 +11,6 @@ interface FlexProps extends HTMLAttributes<HTMLDivElement> {
   shrink?: CSSProperties["flexShrink"];
   wrap?: CSSProperties["flexWrap"];
   gap?: CSSProperties["gap"];
-  margin?: CSSProperties["margin"];
-  marginRight?: CSSProperties["marginRight"];
-  marginTop?: CSSProperties["marginTop"];
-  marginLeft?: CSSProperties["marginLeft"];
-  marginBottom?: CSSProperties["marginBottom"];
-  padding?: CSSProperties["padding"];
-  paddingTop?: CSSProperties["paddingTop"];
-  paddingRight?: CSSProperties["paddingRight"];
-  paddingBottom?: CSSProperties["paddingBottom"];
-  paddingLeft?: CSSProperties["paddingLeft"];
-  border?: CSSProperties["border"];
-  borderRadius?: CSSProperties["borderRadius"];
-  borderColor?: CSSProperties["borderColor"];
-  borderTop?: CSSProperties["borderTop"];
-  borderRight?: CSSProperties["borderRight"];
-  borderBottom?: CSSProperties["borderBottom"];
-  borderLeft?: CSSProperties["borderLeft"];
-  width?: CSSProperties["width"];
-  height?: CSSProperties["height"];
-  position?: CSSProperties["position"];
 }
 
 export const Flex = ({
@@ -44,25 +24,6 @@ export const Flex = ({
   shrink,
   wrap,
   gap,
-  margin,
-  marginRight,
-  marginTop,
-  marginLeft,
-  marginBottom,
-  padding,
-  paddingRight,
-  paddingBottom,
-  paddingLeft,
-  border,
-  borderRadius,
-  borderColor,
-  borderTop,
-  borderRight,
-  borderBottom,
-  borderLeft,
-  width,
-  height,
-  position,
   ...props
 }: FlexProps) => {
   return (
@@ -78,25 +39,6 @@ export const Flex = ({
         flexGrow: grow,
         flexShrink: shrink,
         gap,
-        margin,
-        marginRight,
-        marginTop,
-        marginLeft,
-        marginBottom,
-        padding,
-        paddingRight,
-        paddingBottom,
-        paddingLeft,
-        border,
-        borderRadius,
-        borderColor,
-        borderTop,
-        borderRight,
-        borderBottom,
-        borderLeft,
-        width,
-        height,
-        position,
       }}
       {...props}
     >
