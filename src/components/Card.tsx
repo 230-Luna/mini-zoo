603c07aaab848776cdf5c93666e43adb92559d27
230-Luna <lsy77877@gmail.com>
2025-07-19 20:23:12 +0900

    feat: Homepage 애니메이션 추가

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,6 +2,7 @@ import { ComponentProps } from "react";
 import { fontStyle, Text } from "components/Text";
 import { Icon } from "components/Icon";
 import { Spacing } from "components/Spacing";
+import { AnimationWrapper } from "./AnimationWrapper";
 
 const sizeStyle = {
   standard: { padding: "10px", width: "100px", height: "75px" },
@@ -22,24 +23,26 @@ export const Card = ({
   ...props
 }: CardProps) => {
   return (
-    <div
-      css={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        ...sizeStyle[size],
-      }}
-      {...props}
-    >
-      {description == null ? (
-        <Spacing size={fontStyle.subcation.fontSize} />
-      ) : (
-        <Text typography="subcation">{description}</Text>
-      )}
-      <Spacing size={4} />
-      <Icon name={thumbnail} size={40} />
-      <Spacing size={8} />
-      <Text typography="cation">{title}</Text>
-    </div>
+    <AnimationWrapper type="scaleOnTap">
+      <div
+        css={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          ...sizeStyle[size],
+        }}
+        {...props}
+      >
+        {description == null ? (
+          <Spacing size={fontStyle.subcation.fontSize} />
+        ) : (
+          <Text typography="subcation">{description}</Text>
+        )}
+        <Spacing size={4} />
+        <Icon name={thumbnail} size={40} />
+        <Spacing size={8} />
+        <Text typography="cation">{title}</Text>
+      </div>
+    </AnimationWrapper>
   );
 };
