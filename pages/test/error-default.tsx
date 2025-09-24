a90c6848789cfef7f785f94a63f3f5006074654c
230-Luna <lsy77877@gmail.com>
2025-09-25 02:10:37 +0900

    chore: test 페이지, console.log 제거

--- pages/test/error-default.tsx
+++
@@ -1,9 +0,0 @@
-import ErrorPage from "../_error";
-
-const TestErrorCustomPage = () => {
-  return (
-    <ErrorPage statusCode={403} error={new Error("테스트용 커스텀 에러")} />
-  );
-};
-
-export default TestErrorCustomPage;
